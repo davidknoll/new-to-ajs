e0e34f15746792889b719414b3cf00c2f4f31a02
David Knoll <david@futurefirst.org.uk>
2016-06-16 12:31:31 +0100

    Understanding Isolate Scope

--- app.js
+++ app.js
@@ -106,4 +106,22 @@ angular.module("app", ["ui.router"])
         element.removeClass(attrs.enter);
       });
     };
+  })
+
+  // Understanding Isolate Scope
+  .controller("ChoreCtrl", function ($scope) {
+    $scope.logChore = function (chore) {
+      alert(chore + " is done!");
+    };
+  })
+  .directive("kid", function () {
+    return {
+      restrict: "E",
+      scope: {
+        done: "&"
+      },
+      template: '<input type="text" ng-model="chore"/>' +
+        ' {{chore}}' +
+        ' <div class="button" ng-click="done({chore:chore})">I\'m done!</div>'
+    };
   });
